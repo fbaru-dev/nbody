a9cffc7a1470584bc9476757cfe3947f5ee21e80
Fabio Baruffa <fbaru.dev@gmail.com>
2017-03-31 13:37:07 +0200

    Update GSimulation.cpp

--- nbody-openacc/optimized/GSimulation.cpp
+++ nbody-openacc/optimized/GSimulation.cpp
@@ -17,6 +17,7 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
+
 #include "GSimulation.hpp"
 #include "cpu_time.hpp"
 
